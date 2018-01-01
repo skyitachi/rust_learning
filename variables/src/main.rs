1fca187e609e9b6f10dc45852cc2e3d825a73ad3
skbysp <skbysp@gmail.com>
2018-01-01 12:59:12 +0800

    happy new year 2018

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,9 @@
 fn main() {
-    let x = 5;
+    let mut x = 5;
     println!("Hello, world!");
     println!("x is {}", x);
     x = 10;
+    println!("x is {}", x);
+    let y = 100_0000;
+    println!("y is {}", y);
 }
